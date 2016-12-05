3708605415720d11b1e09535953766052a07ac28
reznyakxD <singyu213@gmail.com>
2016-12-05 23:38:56 +0900

    d

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>Welcome to World</title>
+		<link type="text/css" rel="stylesheet" herf="/css/css.css" />
+	</head>
+	<body>
+		<div id="wrapRoot">
+			<div id="wrapHeader">
+			
+			</div>
+			<div id="wrapContent">
+				<div class="mainImage" >
+					<h4>Imagine yourself in a frozen forest</h4>
+				</div>
+				<div class="mainSidebar">
+				
+				</div>
+				<div class="mainContent">
+				
+				</div>
+			</div>
+			<div id="warpFooter">
+			</div>
+		</div>
+	</body>
+</html>
